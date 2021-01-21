96e173a2878f939155e1695213ee43063771b266
Ashmin <abhandar@ramapo.edu>
2021-01-21 15:29:21 -0500

    Initial Commit

---
+++ trekr/trekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  trekrApp.swift
+//  trekr
+//
+//  Created by Ashmin Bhandari on 1/21/21.
+//
+
+import SwiftUI
+
+@main
+struct trekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
